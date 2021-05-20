5ec43d1aecf47669f19007aff3bd9616827ad116
Mantelioo <80548252+Mantelioo@users.noreply.github.com>
2021-05-20 21:01:25 +0300

    first commit

---
+++ FileBrowser/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace FileBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.LblDriveStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.LblPath = new System.Windows.Forms.ToolStripStatusLabel();
+            this.LstDrives = new System.Windows.Forms.ListBox();
+            this.LstFolderAndFiles = new System.Windows.Forms.ListBox();
+            this.BtnGetDrives = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.LblDriveStatus,
+            this.LblPath});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 381);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(441, 26);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // LblDriveStatus
+            // 
+            this.LblDriveStatus.Name = "LblDriveStatus";
+            this.LblDriveStatus.Size = new System.Drawing.Size(86, 20);
+            this.LblDriveStatus.Text = "Drive status";
+            // 
+            // LblPath
+            // 
+            this.LblPath.Name = "LblPath";
+            this.LblPath.Size = new System.Drawing.Size(30, 20);
+            this.LblPath.Text = "NA";
+            // 
+            // LstDrives
+            // 
+            this.LstDrives.FormattingEnabled = true;
+            this.LstDrives.ItemHeight = 16;
+            this.LstDrives.Location = new System.Drawing.Point(16, 82);
+            this.LstDrives.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.LstDrives.Name = "LstDrives";
+            this.LstDrives.Size = new System.Drawing.Size(159, 276);
+            this.LstDrives.TabIndex = 1;
+            this.LstDrives.SelectedIndexChanged += new System.EventHandler(this.LstDrives_SelectedIndexChanged);
+            this.LstDrives.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.LstDrives_MouseDoubleClick);
+            // 
+            // LstFolderAndFiles
+            // 
+            this.LstFolderAndFiles.FormattingEnabled = true;
+            this.LstFolderAndFiles.ItemHeight = 16;
+            this.LstFolderAndFiles.Location = new System.Drawing.Point(265, 82);
+            this.LstFolderAndFiles.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.LstFolderAndFiles.Name = "LstFolderAndFiles";
+            this.LstFolderAndFiles.Size = new System.Drawing.Size(159, 276);
+            this.LstFolderAndFiles.TabIndex = 2;
+            this.LstFolderAndFiles.SelectedIndexChanged += new System.EventHandler(this.LstFolderAndFiles_SelectedIndexChanged);
+            this.LstFolderAndFiles.KeyDown += new System.Windows.Forms.KeyEventHandler(this.LstFolderAndFiles_KeyDown);
+            this.LstFolderAndFiles.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.LstFolderAndFiles_KeyPress);
+            this.LstFolderAndFiles.KeyUp += new System.Windows.Forms.KeyEventHandler(this.LstFolderAndFiles_KeyUp);
+            this.LstFolderAndFiles.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.LstFolderAndFiles_MouseDoubleClick);
+            // 
+            // BtnGetDrives
+            // 
+            this.BtnGetDrives.Location = new System.Drawing.Point(16, 15);
+            this.BtnGetDrives.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnGetDrives.Name = "BtnGetDrives";
+            this.BtnGetDrives.Size = new System.Drawing.Size(149, 42);
+            this.BtnGetDrives.TabIndex = 3;
+            this.BtnGetDrives.Text = "Get drives";
+            this.BtnGetDrives.UseVisualStyleBackColor = true;
+            this.BtnGetDrives.Click += new System.EventHandler(this.BtnGetDrives_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 407);
+            this.Controls.Add(this.BtnGetDrives);
+            this.Controls.Add(this.LstFolderAndFiles);
+            this.Controls.Add(this.LstDrives);
+            this.Controls.Add(this.statusStrip1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel LblDriveStatus;
+        private System.Windows.Forms.ToolStripStatusLabel LblPath;
+        private System.Windows.Forms.ListBox LstDrives;
+        private System.Windows.Forms.ListBox LstFolderAndFiles;
+        private System.Windows.Forms.Button BtnGetDrives;
+    }
+}
+
---
+++ FileBrowser/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace FileBrowser
+{
+    public partial class Form1 : Form
+    {
+        //Kintamasis disko informacijai sugoti. ji apsirasysime globaliai
+        string CurrentDrive = string.Empty;
+        string PathCopy = "";
+        string PathPaste = "";
+        string ItenName = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGetDrives_Click(object sender, EventArgs e)
+        {
+            //Pirmiausia ka reikia padadaryti tai gauti informacija apie kompiuteryje esancias duomenu laikmenas.
+
+            LstDrives.Items.Clear();
+            string[] Drives = Environment.GetLogicalDrives();
+
+            foreach (var item in Drives)
+            {
+                LstDrives.Items.Add(item);
+            }
+        }
+
+        private void LstDrives_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Sioje funkcijoje mes pasirinksime norima diska ant jo paspaude
+            CurrentDrive = LstDrives.SelectedItem.ToString();
+            DriveInfo Drive = new DriveInfo(CurrentDrive);
+            if (Drive.IsReady)
+            {
+                 LblDriveStatus.Text = ("Free: " + (Drive.AvailableFreeSpace / 1024f / 1024 / 1024).ToString("0.00") + "Gb");
+            }
+            //kadangi informacija rodo baitais, tai mes ja turime dar konvertuoti i gigabaitus
+
+        }
+
+        private void LstDrives_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            //sioje vietoje mes isgausime iar surasysime failu pavadinimus kurie yra direktorijoje i list boxa.
+
+            //Scan selected drive
+            LstFolderAndFiles.Items.Clear();
+            DirectoryInfo Dir = new DirectoryInfo(CurrentDrive);
+            LblPath.Text = CurrentDrive;
+
+            //Isgauname direktorijas ir surasome jas
+            foreach (var item in Dir.GetDirectories())
+            {
+                if (!item.Attributes.HasFlag(FileAttributes.Hidden))
+                {
+                    LstFolderAndFiles.Items.Add(item.Name);
+                }
+            }
+
+            //Isgauname visus failus ir juos surasome i listboxa
+            foreach (var item in Dir.GetFiles())
+            {
+                if (!item.Attributes.HasFlag(FileAttributes.Hidden))
+                {
+                    LstFolderAndFiles.Items.Add(item.Name);
+                }
+            }
+
+        }
+
+        private void LstFolderAndFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //funkcija katalogo informacijai gauti. sudes visus failus ir aplankalus esancius pasrinktoje direktorijoje
+        void ScanFolder(string path)
+        {
+
+            LstFolderAndFiles.Items.Clear();
+            DirectoryInfo Dir = new DirectoryInfo(LblPath.Text);
+
+            //Check if file selected
+            foreach (var item in Dir.GetDirectories())
+            {
+                if (!item.Attributes.HasFlag(FileAttributes.Hidden))
+                {
+                    LstFolderAndFiles.Items.Add(item.Name);
+                }
+            }
+
+            //Get files
+            foreach (var item in Dir.GetFiles())
+            {
+                if (!item.Attributes.HasFlag(FileAttributes.Hidden))
+                {
+                    LstFolderAndFiles.Items.Add(item.Name);
+                }
+            }
+
+        }
+
+        private void LstFolderAndFiles_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+            try
+            {
+                var temp = new DirectoryInfo(LblPath.Text + @"\" + LstFolderAndFiles.SelectedItem.ToString());
+
+                //bandant atidaryti faila mes gauname klaida. Failas juk NERA KELIAS IKI KAZKOKIO OBJEKTO
+                //patikriname ar vartotojas tikrai pasirinko direktorija
+                if (!temp.Attributes.HasFlag(FileAttributes.Directory))
+                {
+                    return;
+                }
+
+                LblPath.Text += LstFolderAndFiles.SelectedItem.ToString() + @"\";
+                //iskvieciam funkcija failams atvaizduoti
+                ScanFolder(LblPath.Text);
+            }
+
+            catch (Exception)
+            {
+
+
+            }
+
+        }
+
+        private void LstFolderAndFiles_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            //jeigu paspaudziama backspace tai griztame atgal. pirmiausia istriname simboli \
+            //tuomet istriname viska iki pries tai buvusio simboli \
+            if (e.KeyCode == Keys.Back)
+            {
+
+                //kad nemestu klaidos jeigu spamintume backspace
+                if (Environment.GetLogicalDrives().Contains(LblPath.Text) || LblPath.Text == "NA")
+                {
+                    return;
+                }
+                else
+                {
+                    LblPath.Text = LblPath.Text.Remove(LblPath.Text.LastIndexOf(@"\"));
+                    LblPath.Text = LblPath.Text.Remove(LblPath.Text.LastIndexOf(@"\") + 1);
+                    ScanFolder(LblPath.Text);
+                }
+
+            }
+
+            //COPY PASTE FUNKCIJA PRIMITYVI. Jei liks laiko, Uzduotis
+
+            if (e.Control && e.KeyCode == Keys.C)
+            {
+                if (LstFolderAndFiles.SelectedItem != null)
+                {
+                    ItenName = LstFolderAndFiles.SelectedItem.ToString();
+                    PathCopy = LblPath.Text + ItenName;
+                    MessageBox.Show(PathCopy);
+                }
+
+            }
+            if (e.Control && e.KeyCode == Keys.V)
+            {
+                PathPaste = LblPath.Text + ItenName;
+                MessageBox.Show(PathPaste);
+                if (PathCopy != "")
+                {
+                    if (!File.Exists(PathPaste) && !(File.GetAttributes(PathCopy) == FileAttributes.Directory))
+                    {
+                        File.Copy(PathCopy, PathPaste);
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("FIle already exists");
+                    }
+                }
+                PathCopy = "";
+                PathPaste = "";
+                ScanFolder(LblPath.Text);
+            }
+
+            //Istrinimo funkcija
+            if (e.KeyCode == Keys.Delete)
+            {
+                DialogResult rez = MessageBox.Show("Ar tikrai norite istrinti faila", "ISTRINTI?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (rez == DialogResult.Yes)
+                {
+                    File.Delete(LblPath.Text + LstFolderAndFiles.SelectedItem);
+                }
+                ScanFolder(LblPath.Text);
+            }
+
+            //OPEN PROGRAM
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (!(File.GetAttributes(LblPath.Text + LstFolderAndFiles.SelectedItem.ToString()) == FileAttributes.Directory))
+                {
+                    Process.Start(LblPath.Text + LstFolderAndFiles.SelectedItem.ToString());
+
+                }
+            }
+
+        }
+
+        private void LstFolderAndFiles_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void LstFolderAndFiles_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+    }
+}
